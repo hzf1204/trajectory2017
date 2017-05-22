7a0d0b74e9ec496a41d36de866cf9d583c0c19a0
hzf1204 <hzf1204@126.com>
2017-05-22 17:01:12 +0800

    22号主页修改

---
+++ app/src/main/java/com/android/trajectory/map/adapter/MyAdapter.java
@@ -0,0 +1,67 @@
+package com.android.trajectory.map.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.trajectory.R;
+import com.android.trajectory.map.shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by mingx_000 on 2016/11/29 0029.
+ */
+
+public class MyAdapter extends BaseAdapter{
+    private Context context;
+    private List<shop> mshop=new ArrayList<>();
+    private LayoutInflater layoutInflater;
+
+    public MyAdapter(Context context, List<shop> mshop) {
+        this.context = context;
+        this.mshop = mshop;
+        this.layoutInflater =LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return mshop.size();
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        shop s=null;
+        if (view==null){
+            s=new shop();
+            view = layoutInflater.inflate(R.layout.map_shop_adapter, null);
+
+            view.setTag(s);
+
+        }
+
+        ImageView Ivsrc=(ImageView)view.findViewById(R.id.img);
+        Ivsrc.setImageResource(Integer.parseInt(mshop.get(i).getSrc()));
+        TextView Tvname=(TextView)view.findViewById(R.id.name);
+        Tvname.setText(mshop.get(i).getName());
+        TextView Tvauther=(TextView)view.findViewById(R.id.message);
+        Tvauther.setText(mshop.get(i).getMessage());
+        return view;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return mshop.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+}
